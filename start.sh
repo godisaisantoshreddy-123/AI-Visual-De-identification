ba74bb3bd66ebabc637e6b0a26543897ad4fd5fd
Sai Santosh Godi Reddy <reddypandu1123@gmail.com>
2025-09-09 16:24:59 +0530

    Fix Render deploy: removed blis and added spacy model download

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Upgrade pip and build tools to make sure we use prebuilt wheels
 pip install --upgrade pip setuptools wheel
-
-# Prefer binaries to avoid building from source
 pip install --prefer-binary -r requirements.txt
-
-# Start Streamlit (adjust filename if your main file is not main.py)
+python -m spacy download en_core_web_sm
 streamlit run main.py --server.port $PORT --server.address 0.0.0.0
