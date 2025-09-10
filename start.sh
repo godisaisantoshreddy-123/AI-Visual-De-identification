83c96e36d089d72394db2956b56b16a4823ad53a
Sai Santosh Godi Reddy <reddypandu1123@gmail.com>
2025-09-10 09:31:19 +0530

    Initial commit

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pip install --upgrade pip setuptools wheel
-pip install --prefer-binary -r requirements.txt
-python -m spacy download en_core_web_sm
-streamlit run main.py --server.port $PORT --server.address 0.0.0.0
